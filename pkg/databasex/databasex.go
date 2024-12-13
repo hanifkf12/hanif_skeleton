b61a566fcd1c614d621b5eedaf2725fe3ca068a7
hanifkf12 <hanifkhoirulfathan@gmail.com>
2024-12-13 08:06:25 +0700

    feat: add transaction db

--- pkg/databasex/databasex.go
+++ pkg/databasex/databasex.go
@@ -15,4 +15,6 @@ type Database interface {
 	Get(ctx context.Context, dst interface{}, query string, args ...interface{}) error
 	Select(ctx context.Context, dst interface{}, query string, args ...interface{}) error
 	Exec(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
+	Transact(ctx context.Context, iso sql.IsolationLevel, txFunc func(database Database) error) (err error)
+	InTransaction() bool
 }
--- pkg/databasex/mysql.go
+++ pkg/databasex/mysql.go
@@ -3,42 +3,117 @@ package databasex
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/hanifkf12/hanif_skeleton/pkg/config"
 	"github.com/jmoiron/sqlx"
 )
 
-type mySql struct {
-	db *sqlx.DB
+type MySql struct {
+	db   *sqlx.DB
+	tx   *sqlx.Tx
+	conn *sqlx.Conn // the Conn of the Tx, when tx != nil
 }
 
-func (m *mySql) Select(ctx context.Context, dst interface{}, query string, args ...interface{}) error {
+func (m *MySql) Select(ctx context.Context, dst interface{}, query string, args ...interface{}) error {
+	if m.tx != nil {
+		return m.tx.SelectContext(ctx, dst, query, args...)
+	}
 	return m.db.SelectContext(ctx, dst, query, args...)
 }
 
-func (m *mySql) Get(ctx context.Context, dst interface{}, query string, args ...interface{}) error {
+func (m *MySql) Get(ctx context.Context, dst interface{}, query string, args ...interface{}) error {
+	if m.tx != nil {
+		return m.tx.GetContext(ctx, dst, query, args...)
+	}
 	return m.db.GetContext(ctx, dst, query, args...)
 }
 
-func (m *mySql) QueryX(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error) {
+func (m *MySql) QueryX(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error) {
+	if m.tx != nil {
+		return m.tx.QueryContext(ctx, query, args...)
+	}
 	return m.db.QueryContext(
 		ctx,
 		query,
 		args...)
 }
 
-func (m *mySql) QueryRowX(ctx context.Context, query string, args ...interface{}) *sql.Row {
+func (m *MySql) QueryRowX(ctx context.Context, query string, args ...interface{}) *sql.Row {
+	if m.tx != nil {
+		return m.tx.QueryRowContext(ctx, query, args...)
+	}
 	return m.db.QueryRowContext(
 		ctx,
 		query, args...)
 }
 
-func (m *mySql) Exec(ctx context.Context, query string, args ...interface{}) (sql.Result, error) {
+func (m *MySql) Exec(ctx context.Context, query string, args ...interface{}) (sql.Result, error) {
+	if m.tx != nil {
+		return m.tx.ExecContext(ctx, query, args...)
+	}
 	return m.db.ExecContext(
 		ctx,
 		query,
 		args...)
+}
+
+func (m *MySql) Transact(ctx context.Context, iso sql.IsolationLevel, txFunc func(database Database) error) (err error) {
+
+	// For the levels which require retry, see
+	// https://www.postgresql.org/docs/11/transaction-iso.html.
+	opts := &sql.TxOptions{Isolation: iso}
+
+	return m.transact(ctx, opts, txFunc)
+}
+
+func (m *MySql) transact(ctx context.Context, opts *sql.TxOptions, txFunc func(database Database) error) (err error) {
+	if m.InTransaction() {
+		return errors.New("db transact function was called on a DB already in a transaction")
+	}
+
+	conn, err := m.db.Connx(ctx)
+	if err != nil {
+		return err
+	}
+
+	defer conn.Close()
+
+	tx, err := conn.BeginTxx(ctx, opts)
+	if err != nil {
+		return fmt.Errorf("tx begin: %w", err)
+	}
+
+	//defer func() {
+	//	if p := recover(); p != nil {
+	//		tx.Rollback()
+	//	} else if err != nil {
+	//		tx.Rollback()
+	//	} else {
+	//		if txErr := tx.Commit(); txErr != nil {
+	//			err = fmt.Errorf("tx commit: %w", txErr)
+	//		}
+	//	}
+	//}()
+
+	mysql := &MySql{
+		db:   m.db,
+		tx:   tx,
+		conn: conn,
+	}
+	//dbtx.opts = *opts
+
+	if err := txFunc(mysql); err != nil {
+		tx.Rollback()
+		return fmt.Errorf("fn(tx): %w", err)
+	}
+
+	return tx.Commit()
+}
+
+func (m *MySql) InTransaction() bool {
+	return m.tx != nil
 }
 
 func NewMySql(cfg *config.Config) (Database, error) {
@@ -48,7 +123,7 @@ func NewMySql(cfg *config.Config) (Database, error) {
 		return nil, err
 	}
 	db.SetMaxOpenConns(10) // Set connection pool limits if needed
-	return &mySql{
+	return &MySql{
 		db: db,
 	}, nil
 }
